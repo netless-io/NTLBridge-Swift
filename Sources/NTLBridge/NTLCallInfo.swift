b0cc34c4047fc3967e0bde463a1f5aa7aa1281bb
vince-m4 <zjxuyunshi@gmail.com>
2025-10-21 15:19:50 +0800

    CallInfo to interval

--- Sources/NTLBridge/NTLCallInfo.swift
+++ Sources/NTLBridge/NTLCallInfo.swift
@@ -1,38 +1,38 @@
 import Foundation
 
 /// Native到JavaScript的调用信息模型，用于编码发往JS的调用
-public struct NTLCallInfo: Codable, Equatable {
+struct NTLCallInfo: Codable, Equatable {
     /// 要调用的JavaScript方法名
-    public let method: String
-    
+    let method: String
+
     /// 回调ID
-    public let callbackId: Int
-    
+    let callbackId: Int
+
     /// 数据载荷（JSON序列化后的字符串）
-    public let data: String
-    
+    let data: String
+
     /// 初始化方法
-    public init(method: String, callbackId: Int, data: String) {
+    init(method: String, callbackId: Int, data: String) {
         self.method = method
         self.callbackId = callbackId
         self.data = data
     }
-      
+
     /// 便捷初始化方法，直接传入Codable类型数据
-    public init<T: Encodable>(method: String, callbackId: Int, codableData: T) throws {
+    init<T: Encodable>(method: String, callbackId: Int, codableData: T) throws {
         self.method = method
         self.callbackId = callbackId
-        
+
         let encoder = NTLBridgeUtil.createEncoder()
         let data = try encoder.encode(codableData)
         self.data = String(data: data, encoding: .utf8) ?? "{}"
     }
-    
+
     /// 便捷初始化方法，直接传入可选Codable类型数据
-    public init<T: Encodable>(method: String, callbackId: Int, codableData: T?) throws {
+    init<T: Encodable>(method: String, callbackId: Int, codableData: T?) throws {
         self.method = method
         self.callbackId = callbackId
-        
+
         if let codableData = codableData {
             let encoder = NTLBridgeUtil.createEncoder()
             let data = try encoder.encode(codableData)
